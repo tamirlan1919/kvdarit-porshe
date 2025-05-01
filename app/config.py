caaaa09aea0d0b83bb304fdd3271c9301fae8dd4
Timerlan Chinchaev <tchinchaev@bk.ru>
2025-05-01 21:40:32 +0300

    обновил бд

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///participants.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', "postgresql://gen_user:%3BC%3C4ju%7B-XC_%7BN4@92.53.107.41:5432/default_db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
